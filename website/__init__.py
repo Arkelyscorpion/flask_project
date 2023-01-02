07b0586f4a7f132bf660b4fe6ac80aa2df70e521
Krishna Sameer <krishnasameer51@gmail.com>
2023-01-02 20:56:12 +0530

    Added the Form field validation, added flash messages, modified the base.html

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = '00000121asddfja91jalLKLKJ()134i'
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+
     from .views import views
     from .auth import auth
 
--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint,render_template,request, flash
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login',methods=['GET','POST'])
 def login():
-    return "<p>Login</p>"
+    return render_template('login.html')
 
 
 
@@ -14,6 +14,24 @@ def logout():
 
 
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
-    return "<p>sign up</p>"
+
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters',category='error')
+        elif len(firstName) < 2:
+            flash('First Name must be atleast more than 2 characters',category='error')
+        elif password1 != password2:
+            flash('ERROR Passwords do not match',category='error')
+        elif len(password1) < 7:
+            flash('Password is too short',category='error')
+        else:
+            flash('Account Created Successfully',category='success')
+            
+    return render_template('sign_up.html')
