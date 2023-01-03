0ea7fa3baa47a61a13f22e0e114f77997b5daf76
Krishna Sameer <krishnasameer51@gmail.com>
2023-01-03 09:23:49 +0530

    Added the notes HTML form in the home page

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from flask_login import LoginManager
+
+
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
@@ -12,6 +15,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
+    
 
     from .views import views
     from .auth import auth
@@ -23,6 +27,14 @@ def create_app():
     with app.app_context():
         db.create_all()
 
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     
     return app
     
